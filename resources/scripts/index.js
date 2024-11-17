ecd8514477cd11e54c43d089bbebeedcd4854cfa
Zack <pantchzk@mail.uc.edu>
2024-11-16 22:40:20 -0500

    Used on-save action to fix fixable errors on save

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -10,8 +10,8 @@ const gameHistoryParagraph = document.getElementById(`game-history`);
 const resetGameButton = document.getElementById(`reset-game-button`);
 
 // instantiate the game object from the `RockPaperScissors` class.
-var game;
-var username;
+let game;
+let username;
 
 // hide gamescreen
 gameScreen.classList.add(`d-none`);
@@ -53,13 +53,13 @@ goButton.addEventListener(`click`, function (e) {
 });
 
 //reset-game-button
-resetGameButton.addEventListener('click', function(e) { 
-     e.preventDefault();
-  userName.value = '';
-     username = '';
+resetGameButton.addEventListener(`click`, function(e) { 
+  e.preventDefault();
+  userName.value = ``;
+  username = ``;
   game = new RockPaperScissors(username);
 
   welcomeScreen.classList.remove(`d-none`);
-     gameScreen.classList.add(`d-none`);
+  gameScreen.classList.add(`d-none`);
   resetGameButton.classList.add(`d-none`);
 })
