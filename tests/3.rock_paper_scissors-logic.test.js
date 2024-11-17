09f16af08380b67cf48f0d5f127e35ab6d93785b
Zack <pantchzk@mail.uc.edu>
2024-11-16 23:59:23 -0500

    Finished implementing unit test 3

--- tests/3.rock_paper_scissors-logic.test.js
+++ tests/3.rock_paper_scissors-logic.test.js
@@ -5,15 +5,25 @@ describe(`RockPaperScissors class`, function () {
     test(`win cases`, function () {
       const game = new RockPaperScissors();
       expect(game.determineWinner(`rock`, `scissors`)).toBe(`win`);
+      expect(game.determineWinner(`paper`, `rock`)).toBe(`win`);
+      expect(game.determineWinner(`scissors`, `paper`)).toBe(`win`);
       // Complete the test
     });
 
     test(`tie cases`, function () {
       // Write your test here
+      const game = new RockPaperScissors();
+      expect(game.determineWinner(`rock`, `rock`)).toBe(`tie`);
+      expect(game.determineWinner(`paper`, `paper`)).toBe(`tie`);
+      expect(game.determineWinner(`scissors`, `scissors`)).toBe(`tie`);
     });
 
     test(`lost cases`, function () {
       // Write your test here
+      const game = new RockPaperScissors();
+      expect(game.determineWinner(`rock`, `paper`)).toBe(`lose`);
+      expect(game.determineWinner(`paper`, `scissors`)).toBe(`lose`);
+      expect(game.determineWinner(`scissors`, `rock`)).toBe(`lose`);
     });
   });
 });
